bb244d6de2a0dbd87d959ec5e882ac5c5037db81
sakhdlam <sakhiledlamin@yahoo.com>
2015-07-30 14:20:41 -0700

    Update menucontroller.js

--- scripts/menucontroller.js
+++ scripts/menucontroller.js
@@ -1,11 +1,10 @@
 MenuController = function() {
-  debugger;
-  this.menu = document.getElementsByClassName('menu');
 }
 
 /**
  * Shows up the menu by setting the visibility of the menu to visible.
  */
 MenuController.prototype.showMenu = function() {
-  this.menu[0].style.visibility = "visible";
+  this.menu = document.getElementsByClassName('menu')[0];
+  this.menu.style.visibility = "visible";
 }
