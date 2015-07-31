e2a51b7afefcdef0a6c917453f1ddada56a713ca
sakhdlam <sakhiledlamin@yahoo.com>
2015-07-30 18:16:35 -0700

    Update menucontroller.js

--- scripts/menucontroller.js
+++ scripts/menucontroller.js
@@ -8,5 +8,5 @@ MenuController.prototype.showMenu = function() {
   this.menu = document.getElementsByClassName('menu-container')[0];
   this.menu.classList.toggle('show-menu');
   this.menu.children[0].focus();
-  this.menu.setAttribute("aria-activedescendant", this.menu.children[0]);
+  this.menu.setAttribute("aria-activedescendant", "firstItem");
 }
